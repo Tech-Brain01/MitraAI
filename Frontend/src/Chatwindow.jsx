7edffb4106f350a4285410fc00d5a37765bc5d71
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-08-22 00:21:11 +0530

    frontend done delete and few features left

---
+++ Frontend/src/Chatwindow.jsx
@@ -0,0 +1,89 @@
+import Chat from "./Chat";
+import "./ChatWindow.css";
+import { Mycontext } from "./MyContext.jsx";
+import { useContext , useState , useEffect } from "react";
+import {ScaleLoader} from "react-spinners";
+
+function ChatWindow() {
+  const { prompt, setPrompt, reply, setReply , currThreadId , prevChats , setPrevChats , setNewChat } = useContext(Mycontext);
+  const [loading, setLoading] = useState(false);
+
+
+
+  const getReply = async () => {
+    setLoading(true);
+    setNewChat(false);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:  JSON.stringify({
+        message: prompt,
+        threadId: currThreadId
+      })
+    };
+ 
+      try {
+        const response =  await fetch("http://localhost:8080/api/chat", options);
+        const res = await response.json();
+        console.log(res);
+        setReply(res.reply);
+      } catch (error) {
+        console.error("Error fetching chat response:", error);
+      }
+     setLoading(false);
+  }
+
+  useEffect(() => {
+     if(prompt && reply) {
+      setPrevChats(prevChats => (
+        [...prevChats,{
+          role: "user",
+          content: prompt
+        },
+      {
+        role: "assistant",
+        content: reply
+      }]
+      ))
+     }
+
+     setPrompt("");
+  }, [reply]);
+
+  return (
+    // Add flex-1 to make this component grow and fill available space
+    <div className="chatWindow">
+      <div className="navbar">
+        <span>
+          MitraAi &nbsp; <i className="fa-solid fa-chevron-down"></i>
+        </span>
+        <div className="userIconDiv">
+          <span className="userIcon">
+            <i className="fa-solid fa-user"></i>
+          </span>
+        </div>
+      </div>
+      <Chat></Chat>
+      <ScaleLoader color="#fff" loading={loading}>
+
+      </ScaleLoader>
+      <div className="chatInput">
+        <div className="inputBox">
+          <input
+            placeholder="Ask MitraAI"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' ? getReply() : ' '}
+          ></input>
+          <div id="submit" className="fa-solid fa-paper-plane" onClick={getReply}></div>
+        </div>
+        <p className="info">MitraAI can make mistakes. check Important Info.</p>
+      </div>
+    </div>
+  );
+}
+
+export default ChatWindow;
---
+++ Frontend/src/Sidebar.jsx
@@ -0,0 +1,81 @@
+import "./Sidebar.css";
+import { useContext , useEffect } from "react";
+import { Mycontext } from "./MyContext.jsx";
+import { v1 as uuidv1 } from 'uuid';
+
+function Sidebar() {
+  const { allThreads, setAllThreads,  currThreadId , setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats } = useContext(Mycontext);
+
+  const getAllThreads = async () => {
+
+     try {
+      const response = await fetch("http://localhost:8080/api/thread");
+      const res = await response.json();
+      const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
+      // console.log(filteredData);
+      setAllThreads(filteredData);
+      // console.log(res);
+
+     } catch (err) {
+      console.log(err);
+     }
+   };
+
+  useEffect(() => {
+    getAllThreads();
+  }, [currThreadId]);
+
+  const createNewChat = () => {
+    setNewChat(true);
+    setPrompt("");
+    setReply(null);
+    setCurrThreadId(uuidv1());
+    setPrevChats([]);
+  }
+
+  const changeThread = async(newThreadId) => {
+    setCurrThreadId(newThreadId);
+
+    try {
+      const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+      const res = await response.json();
+      // console.log(res);   
+      setPrevChats(res.messages || []);
+      setNewChat(false);
+      setReply(null);
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <section className="sidebar">
+
+      {/* new chat button */}
+      <button onClick={createNewChat}>
+          <img className="logo" src = "src/assets/logo.png" alt="MitraAI logo" ></img>
+          <span><i className="fa-solid fa-pen-to-square"></i></span>
+      </button>
+
+        {/* history */}
+
+        <ul className="history">
+            {
+               allThreads?.map((thread, idx) => (
+                 <li key={idx}
+                  onClick={(e) => changeThread(thread.threadId)}
+                 > {thread.title} </li>
+               ))
+            }
+        </ul>
+
+          {/* sign */}
+             
+            <div className="sign"></div>
+                <p>By Amrendera &hearts;</p>
+    </section>
+ )
+}
+
+export default Sidebar;
